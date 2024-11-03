65a6876035d38c90e569e0f7bfe94742fb8e9b12
Sebastian Green <sebastianpetergreen@outlook.com>
2024-11-03 18:12:07 +0100

    MudBlazor reference, unnecessary imports

--- SatisfactoryAccounting.Client/Program.cs
+++ SatisfactoryAccounting.Client/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using SatisfactoryAccounting.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,6 +8,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMudServices();
 builder.Services.AddSingleton<SatisfactoryModelProvider>();
 
 await builder.Build().RunAsync();
